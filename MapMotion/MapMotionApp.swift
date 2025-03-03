403ecddcea09d7685ffe5966e1fd74299f8b20a5
alaaiosdev <alaa.ajoury94@gmail.com>
2025-03-03 14:46:57 +0300

    Initial Commit

---
+++ MapMotion/MapMotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapMotionApp.swift
+//  MapMotion
+//
+//  Created by Alaa Abu-Taha on 03/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct MapMotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
